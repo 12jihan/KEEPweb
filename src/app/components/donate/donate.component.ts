8026b1601aa074bedff0440f0578fbda860aa0a5
12jikan <jareem.e.hoff@gmail.com>
2018-08-06 00:23:11 -0400

    not sure what's happening rn

--- src/app/components/donate/donate.component.ts
+++ src/app/components/donate/donate.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-donate',
   templateUrl: './donate.component.html',
   styleUrls: ['./donate.component.css']
 })
 export class DonateComponent implements OnInit {
+
+  faHt = faHeart;
 
   constructor() { }
 
